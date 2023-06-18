455774df85c07f03207eafcbd69fe5c96a7ac43a
yeferson <131elyefer@gmail.com>
2023-06-17 21:56:01 -0500

    version 1.4

--- app/components/trending/Trending.tsx
+++ app/components/trending/Trending.tsx
@@ -82,6 +82,7 @@ export default function Trending() {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                         </svg>
                     </div>
+                    
                 )}
 
             </div>
